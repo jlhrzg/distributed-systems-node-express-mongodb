e798a5ece34672d65fd5ad785632e33823b41a57
jlhrzg <90400632+jlhrzg@users.noreply.github.com>
2022-05-08 18:01:38 +0200

    Add note html view

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
+const mongoUri = process.env.MONGO_URL || 'mongodb://localhost:27017/';
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/');
+    const conn = await mongoose.connect(mongoUri);
     console.log("Connected to database");
   } catch (error) {
     console.log(error);
